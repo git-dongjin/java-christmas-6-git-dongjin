152d821a0bd3b0c703242e8881ac52102d4b6210
unknown <y2kf9a0q@gmail.com>
2023-11-10 03:56:40 +0900

    feat: Badge.java - 배지 열거형

--- src/main/java/christmas/enums/Badge.java
+++ src/main/java/christmas/enums/Badge.java
@@ -1,5 +1,16 @@
 package christmas.enums;
 
 public enum Badge {
-    STAR, TREE, SANTA, BLANK
+    SANTA("산타", 20_000),
+    TREE("트리", 10_000),
+    STAR("별", 5_000),
+    BLANK("없음", 0);
+
+    private final String name;
+    private final int minProfit;
+
+    Badge(String name, int minProfit) {
+        this.name = name;
+        this.minProfit = minProfit;
+    }
 }
