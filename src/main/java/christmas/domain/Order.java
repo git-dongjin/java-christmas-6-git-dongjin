51cd410c57434a3279dfe39384648d00763d169d
unknown <y2kf9a0q@gmail.com>
2023-11-10 09:13:29 +0900

    feat: Order.java - Orders의 단위

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -6,8 +6,26 @@ public class Order {
     private final Menu menu;
     private final int count;
 
-    public Order(Menu menu, int count) {
-        this.menu = menu;
+    public Order(String menu, int count) {
+        validateExistMenu(menu);
+        validateNotZeroNegative(count);
+        this.menu = convertStringToMenu(menu);
         this.count = count;
+    }
+
+    private void validateExistMenu(String menu) {
+        if (!Menu.contains(menu)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateNotZeroNegative(int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private Menu convertStringToMenu(String menu) {
+        return Menu.convertStringToMenu(menu);
     }
 }
