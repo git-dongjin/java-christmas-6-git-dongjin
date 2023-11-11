fcb536c8090bdec483a69a57258f4d26f1a423d8
unknown <y2kf9a0q@gmail.com>
2023-11-12 02:43:43 +0900

    refactor: ViewController 구현

--- src/main/java/christmas/controller/ViewController.java
+++ src/main/java/christmas/controller/ViewController.java
@@ -1,4 +1,55 @@
 package christmas.controller;
 
+import christmas.domain.Day;
+import christmas.domain.order.OrdersMenuCount;
+import christmas.view.input.InputParser;
+import christmas.view.input.InputView;
+import christmas.view.input.Reader;
+import christmas.view.output.OutputView;
+import christmas.view.output.Writer;
+
 public class ViewController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public ViewController(Reader reader, InputParser inputParser, Writer writer) {
+        this.outputView = new OutputView(writer);
+        this.inputView = new InputView(reader, inputParser, outputView);
+    }
+
+    public Day inputDate() {
+        return inputView.readDate();
+    }
+
+    public OrdersMenuCount inputOrdersMenuCount() {
+        return inputView.readOrdersMenuCount();
+    }
+
+    public void outputNewLine() {
+        outputView.printNewLine();
+    }
+
+    public void outputWelcome() {
+        outputView.printWelcome();
+    }
+
+    public void outputReadDate() {
+        outputView.printReadDate();
+    }
+
+    public void outputReadOrdersMenuCount() {
+        outputView.printReadOrdersMenuCount();
+    }
+
+    public void outputReadDateError() {
+        outputView.printReadDateError();
+    }
+
+    public void outputReadOrdersMenuCountError() {
+        outputView.printReadOrdersMenuCountError();
+    }
+
+    public void output(Object object) {
+        outputView.print(object);
+    }
 }
