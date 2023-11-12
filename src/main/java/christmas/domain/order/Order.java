1dcc016519b47af3478e368a5089aa907f77c69b
unknown <y2kf9a0q@gmail.com>
2023-11-12 21:29:21 +0900

    refactor: Order - getMenu, getCount 삭제

--- src/main/java/christmas/domain/order/Order.java
+++ src/main/java/christmas/domain/order/Order.java
@@ -1,24 +1,44 @@
 package christmas.domain.order;
 
 import christmas.enums.Menu;
+import org.mockito.internal.matchers.Or;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Order {
     private final Menu menu;
     private final int count;
+
+    public static Map<Menu, Integer> convertListToMap(List<Order> orders) {
+        validateMenuNotDuplicates(orders);
+        Map<Menu, Integer> orderMap = new LinkedHashMap<>();
+
+        for (Order order : orders) {
+            orderMap.put(order.menu, order.count);
+        }
+
+        return Map.copyOf(orderMap);
+    }
+
+    public static void validateMenuNotDuplicate(List<Order> orders) {
+        if (orders.size() != orders.stream().map(order -> order.menu).distinct().count()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validateMenuNotDuplicates(List<Order> orders) {
+        if (orders.size() != orders.stream().map(order -> order.menu).distinct().count()) {
+            throw new IllegalStateException();
+        }
+    }
 
     public Order(String menu, int count) {
         validateExistMenu(menu);
         validateNotZeroNegative(count);
         this.menu = convertStringToMenu(menu);
         this.count = count;
-    }
-
-    public Menu getMenu() {
-        return menu;
-    }
-
-    public int getCount() {
-        return count;
     }
 
     private void validateExistMenu(String menu) {
--- src/main/java/christmas/domain/order/Orders.java
+++ src/main/java/christmas/domain/order/Orders.java
@@ -2,7 +2,6 @@ package christmas.domain.order;
 
 import christmas.enums.Menu;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -10,22 +9,12 @@ public class Orders {
     private final List<Order> orders;
 
     public Orders(List<Order> orders) {
-        validateMenuNotDuplicate(orders);
+        Order.validateMenuNotDuplicate(orders);
 
         this.orders = orders;
     }
 
-    private void validateMenuNotDuplicate(List<Order> orders) {
-        if (orders.size() != orders.stream().map(Order::getMenu).distinct().count()) {
-            throw new IllegalArgumentException();
-        }
-    }
-
     public Map<Menu, Integer> convertListToMap() {
-        Map<Menu, Integer> orderMap = new LinkedHashMap<>();
-        for(Order order : orders) {
-            orderMap.put(order.getMenu(), order.getCount());
-        }
-        return Map.copyOf(orderMap);
+        return Order.convertListToMap(orders);
     }
 }
