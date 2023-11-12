8feedd1eb42dccc8b3d4da5b72fc8b295d1edfb5
unknown <y2kf9a0q@gmail.com>
2023-11-12 10:03:51 +0900

    feat: ProfitTotal - 총혜택 금액, 증정 제외 혜택금액

--- src/main/java/christmas/domain/money/ProfitTotal.java
+++ src/main/java/christmas/domain/money/ProfitTotal.java
@@ -1,9 +1,32 @@
 package christmas.domain.money;
 
-public class ProfitTotal {
-    private final Money money;
+import christmas.domain.policy.ProfitPolicies;
+import christmas.domain.policy.ProfitPolicy;
 
-    public ProfitTotal(Money money) {
-        this.money = money;
+public class ProfitTotal {
+    private final ProfitPolicies profitPolicies;
+
+    public ProfitTotal(ProfitPolicies profitPolicies) {
+        this.profitPolicies = profitPolicies;
+    }
+
+    public Money getTotalProfit() {
+        Money totalProfit = Money.ZERO;
+
+        for (ProfitPolicy profitPolicy : profitPolicies) {
+            totalProfit = totalProfit.add(profitPolicy.getProfit());
+        }
+
+        return totalProfit;
+    }
+
+    public Money getTotalProfitExceptPresent() {
+        return new ProfitTotal(profitPolicies.getProfitPoliciesExceptPresentPolicies()).getTotalProfit();
+    }
+
+    @Override
+    public String toString() {
+        return "<총혜택 금액>" + System.lineSeparator()
+                + getTotalProfit() + "원" + System.lineSeparator();
     }
 }
--- src/main/java/christmas/domain/policy/ProfitPolicies.java
+++ src/main/java/christmas/domain/policy/ProfitPolicies.java
@@ -1,8 +1,9 @@
 package christmas.domain.policy;
 
+import java.util.Iterator;
 import java.util.List;
 
-public class ProfitPolicies {
+public class ProfitPolicies implements Iterable<ProfitPolicy> {
     private final List<ProfitPolicy> profitPolicies;
 
     public ProfitPolicies(List<ProfitPolicy> profitPolicies) {
@@ -10,9 +11,18 @@ public class ProfitPolicies {
         this.profitPolicies = profitPolicies;
     }
 
+    public ProfitPolicies getProfitPoliciesExceptPresentPolicies() {
+        return new ProfitPolicies(profitPolicies.stream().filter(value -> !(value instanceof PresentPolicy)).toList());
+    }
+
     private void validateNotDuplicateClasses(List<ProfitPolicy> profitPolicies) {
         if (profitPolicies.size() != profitPolicies.stream().map(ProfitPolicy::getClass).distinct().count()) {
             throw new IllegalStateException();
         }
+    }
+
+    @Override
+    public Iterator<ProfitPolicy> iterator() {
+        return profitPolicies.iterator();
     }
 }
