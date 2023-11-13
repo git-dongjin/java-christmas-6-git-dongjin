e64d22fc25b2969c715b6ef6771e9b773dc84eed
unknown <y2kf9a0q@gmail.com>
2023-11-14 06:32:28 +0900

    test: Day - 생성자

--- src/test/java/christmas/domain/unit/DayTest.java
+++ src/test/java/christmas/domain/unit/DayTest.java
@@ -1,20 +1,31 @@
 package christmas.domain.unit;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class DayTest {
     private static final String LINE_SEPARATOR = System.lineSeparator();
     private static final int DAY = 3;
     private Day day;
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31})
+    void constructor_성공(int day) {
+        assertThatCode(() -> new Day(day)).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 32})
+    void constructor_예외발생(int day) {
+        assertThatThrownBy(() -> new Day(day)).isInstanceOf(IllegalArgumentException.class);
+    }
 
     @ParameterizedTest
     @CsvSource(value = {"2,false", "4,true", "3,false"})
