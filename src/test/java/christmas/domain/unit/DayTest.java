c06864f75e941503e90406cf852d25e2dc6a011b
unknown <y2kf9a0q@gmail.com>
2023-11-14 07:14:15 +0900

    test: Money

--- src/test/java/christmas/domain/unit/DayTest.java
+++ src/test/java/christmas/domain/unit/DayTest.java
@@ -1,12 +1,10 @@
 package christmas.domain.unit;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.mock;
 
 class DayTest {
     private static final String LINE_SEPARATOR = System.lineSeparator();
